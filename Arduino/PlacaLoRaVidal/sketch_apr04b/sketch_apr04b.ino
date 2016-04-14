1a6ddf04aab194d76ce76f2a728c2ed87bf957f6
Luca Pasquale <lucapasquale@hotmail.com>
2016-04-14 19:33:06 -0300

    Incluido modoDebug

--- Arduino/PlacaLoRaVidal/sketch_apr04b/sketch_apr04b.ino
+++ Arduino/PlacaLoRaVidal/sketch_apr04b/sketch_apr04b.ino
@@ -1,148 +1,168 @@
 #include <SoftwareSerial.h>
 SoftwareSerial Serial1(2, 3);
 
+#define modoDebug;
+#define delayMessage 10000
+
 int counter;
 
 void setup() {
+#ifdef modoDebug
   Serial.begin(115200);
-  Serial1.begin(57600);
+#endif
 
-  delay(2000);
+  Serial1.begin(57600);
   LoRaConfig();
-  Serial.println("------------------------------");
-  Serial.println("------------------------------");
-  delay(2000);
 }
 
 void loop() {
   counter++;
-  LoRaSendAndReceive(String(counter));
-  delay(30000);
+  LoRaSendUncnf(counter);
+  delay(delayMessage);
 }
 
-void LoRaSendAndReceive(String data)
+void LoRaSendUncnf(int data)
 {
+  LoRaWriteGpio(5, 1);
+  delay(50);
+
+  LoRaWriteGpio(5, 0);
+  delay(950);
+
+#ifdef modoDebug
   Serial.println("");
   Serial.print("Sending: ");
   Serial.println(data);
+#endif
 
+  Serial.write("mac tx uncnf 1 ");
+  Serial.print(data);
+  Serial.write("\r\n");
 
-  Serial1.write("mac tx uncnf 1 ");
-  Serial1.print(data);
-  Serial1.write("\r\n");
+  WaitResponse(1000);                             //receive ok from module
 
-  delay(1000);
+  WaitResponse(5000);                         //receive mac_tx_ok from data sent
+
+  LoRaWriteGpio(5, 1);
+  delay(50);
+
+  LoRaWriteGpio(5, 0);
+  delay(950);
+
+}
+
+void WaitResponse(int timeDelay)
+{
+  delay(timeDelay);
+
+  //// aqui mostra o retorno do modulo. devo ler e se nao for OK, joined, ... resetar
+#ifdef modoDebug
   while (Serial1.available()) Serial.write(Serial1.read());
-  delay(5000);
-  while (Serial1.available()) Serial.write(Serial1.read());
+  Serial.println("");
+#endif
 }
 
 void LoRaConfig()
 {
-  Serial.print("Reset:\t\t");                                                                                //Device reset
+  delay(2000);
+#ifdef modoDebug
+  Serial.print("Reset: ");
+#endif
   Serial1.write("sys reset\r\n");
-  delay(1000);
-  while (Serial1.available()) Serial.write(Serial1.read());
-  LoRaBlink(500);
+  WaitResponse(2000);
 
-  //Receive device model
-  Serial.print("Device:\t\t");
-  Serial1.write("sys get ver\r\n");
-  delay(50);
-  while (Serial1.available()) Serial.write(Serial1.read());
-  LoRaBlink(500);
+  //#ifdef modoDebug
+  //  Serial.print("Set DevEui: ");
+  //#endif
+  //  Serial1.write("mac set deveui 0004A300020155A0\r\n");
+  //  WaitResponse(1000);
 
-  Serial.print("EUI:\t\t");                                                                                  //
-  Serial1.write("sys get hweui\r\n");
-  delay(50);
-  while (Serial1.available()) Serial.write(Serial1.read());
-  LoRaBlink(500);
+#ifdef modoDebug
+  Serial.print("Set DevADDr/NwkAddr: ");
+#endif
+  Serial1.write("mac set devaddr 020155A0\r\n");
+  WaitResponse(1000);
 
-  Serial.print("addr:\t\t");                                                                                 //Set device address for your device!!!!!!!!!!!
-  Serial1.write("mac set devaddr 020155B0\r\n");
-  delay(50);
-  while (Serial1.available()) Serial.write(Serial1.read());
-  LoRaBlink(500);
+#ifdef modoDebug
+  Serial.print("Set AppSkey: ");
+#endif
+  Serial1.write("mac set appskey 2B7E151628AED2A6ABF7158809CF4F3C\r\n");
+  WaitResponse(1000);
 
-  Serial.print("nwkskey:\t");                                                                              //Set NwkSkey key for your device!!!!!!!!!!!
+#ifdef modoDebug
+  Serial.print("Set NwkSkey: ");
+#endif
   Serial1.write("mac set nwkskey 2B7E151628AED2A6ABF7158809CF4F3C\r\n");
-  delay(50);
-  while (Serial1.available()) Serial.write(Serial1.read());
-  LoRaBlink(500);
+  WaitResponse(1000);
 
-  Serial.print("appskey:\t");
-  Serial1.write("mac set appskey 2B7E151628AED2A6ABF7158809CF4F3C\r\n");                                  //Set AppSkey for your device!!!!!!!!!!
-  delay(50);
-  while (Serial1.available()) Serial.write(Serial1.read());
-  LoRaBlink(500);
-
-  Serial.print("adr:\t\t");
+#ifdef modoDebug
+  Serial.print("Set ADR On: ");
+#endif
   Serial1.write("mac set adr on\r\n");
-  delay(50);
-  while (Serial1.available()) Serial.write(Serial1.read());
-  LoRaBlink(500);
+  WaitResponse(1000);
 
-  //  Serial.print("data rate: ");
-  //  Serial1.write("mac get dr\r\n");
-  //  delay(50);
-  //  while (Serial1.available()) Serial.write(Serial1.read());
-  //  LoRaBlink(500);
-  //
-  //  Serial.print("ch: ");
-  //  Serial1.write("mac get ch\r\n");
-  //  delay(50);
-  //  while (Serial1.available()) Serial.write(Serial1.read());
-  //  LoRaBlink(500);
-  //
-  //  Serial.print("band: ");
-  //  Serial1.write("mac get band\r\n");
-  //  delay(50);
-  //  while (Serial1.available()) Serial.write(Serial1.read());
-  //  LoRaBlink(500);
+#ifdef modoDebug
+  Serial.print("Set Pwr: ");
+#endif
+  Serial1.write("radio set pwr 20\r\n");
+  WaitResponse(1000);
 
-  //  Serial.print("rx delay1: ");
-  //  Serial1.write("mac get rxdelay1\r\n");
-  //  delay(50);
-  //  while (Serial1.available()) Serial.write(Serial1.read());
-  //  LoRaBlink(500);
-  //
-  //  Serial.print("rx delay2: ");
-  //  Serial1.write("mac get rxdelay2\r\n");
-  //  delay(50);
-  //  while (Serial1.available()) Serial.write(Serial1.read());
-  //  LoRaBlink(500);
+  // Canais off - Ex.: mac set ch status 24 off<0d0a>
+#ifdef modoDebug
+  Serial.println("Desabilitando canais 8 a 71");
+#endif
+  for (int loop = 8; loop < 72; loop++)      // desligo do 8 ao 71
+  {
+    Serial1.write("mac set ch status ");
+    Serial1.print(loop);
+    Serial1.write(" off\r\n");
 
-  Serial.print("Pwr:\t\t");
-  Serial1.write("mac set pwridx 10\r\n");
-  delay(50);
-  while (Serial1.available()) Serial.write(Serial1.read());
-  LoRaBlink(500);
+    WaitResponse(200);
 
-  Serial.print("Saved\t\t");
+#ifdef modoDebug
+    Serial.print("Canal ");
+    Serial.print(loop);
+    Serial.println(" desativado.");
+#endif
+  }
+
+#ifdef modoDebug
+  Serial.print("SAVE: ");
+#endif
   Serial1.write("mac save\r\n");
-  delay(1000);
-  while (Serial1.available()) Serial.write(Serial1.read());
-  LoRaBlink(500);
+  WaitResponse(1000);
 
-  Serial.print("Join:\t\t");
+#ifdef modoDebug
+  Serial.print("Join: ");
+#endif
   Serial1.write("mac join abp\r\n");
-  delay(500);
-  while (Serial1.available()) Serial.write(Serial1.read());
-  LoRaBlink(500);
+  WaitResponse(1000);
 
-  //  Serial.print("status: ");
-  //  Serial1.write("mac get status\r\n");
-  //  delay(50);
-  //  while (Serial1.available()) Serial.write(Serial1.read());
-  //  LoRaBlink(500);
+#ifdef modoDebug
+  Serial.println("------------------------------");
+  Serial.println("-------LoRa Configurado-------");
+  Serial.println("------------------------------");
+#endif
+  delay(2000);
 }
 
-void LoRaBlink(int timeOn) {
-  Serial1.write("sys set pindig GPIO5 1\r\n");
-  delay(timeOn);
-  while (Serial1.available()) Serial1.read();
+void LoRaWriteGpio(int gpiopin, int state)
+{
+#ifdef modoDebug
+  Serial.print("GPIO ");
+  Serial.print(gpiopin);
+  if (state == 0)
+    Serial.println(" desligado");
+  else
+    Serial.println(" ligado");
+#endif
 
-  Serial1.write("sys set pindig GPIO5 0\r\n");
-  delay(50);
-  while (Serial1.available()) Serial1.read();
+  Serial.write("sys set pindig GPIO");
+  Serial.print(gpiopin);
+  if (state == 0)
+    Serial.write(" 0\r\n");
+  else
+    Serial.write(" 1\r\n");
+
+  WaitResponse(200);                             //receive ok from module
 }

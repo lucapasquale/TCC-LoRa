6f6fd80f3f8664a96af689e1f0230bbd4c9e23b8
Luca Pasquale <lucapasquale@hotmail.com>
2016-06-03 15:07:17 -0300

    alterado para pegar todos os dataFrames e ordenar por data recebida

--- Python/teste requests.py
+++ Python/teste requests.py
@@ -1,23 +1,25 @@
 import json, requests, base64, csv
 
-url = 'https://artimar.orbiwise.com/rest/nodes/0004A30B001A674B/payloads/ul/latest'
+url = 'https://artimar.orbiwise.com/rest/nodes/0004A30B001A674B/payloads/ul'
 
+## Conecta no API da orbiwise, ignorando a verificacao HTTPS
 from requests.auth import HTTPBasicAuth
 r = requests.get(url, auth=HTTPBasicAuth('maua_ceun', 'Maua2016'), verify = False)
 
+## Pega o JSON e o ordena pela data recebida
 json_string = r.text
 parsed_json = json.loads(json_string)
+jsonSorted = sorted(parsed_json, key=lambda k: k['timestamp'])
 
-dataFrame = parsed_json['dataFrame']
-dataFrame = base64.b64decode(dataFrame)
-dataFrame = base64.b16encode(dataFrame)
+## Decodifica os dados de base64 para HEX
+for l in range(0,len(jsonSorted)):
+    jsonSorted[l]["dataFrame"] = base64.b64decode(jsonSorted[l]["dataFrame"])
+    jsonSorted[l]["dataFrame"] = base64.b16encode(jsonSorted[l]["dataFrame"])
 
-port = parsed_json['port']
-timestamp = parsed_json['timestamp']
-fcnt = parsed_json['fcnt']
-
+## Grava como CSV
 outputFile = open('testeCSV.csv', 'w')
 outputWriter = csv.writer(outputFile)
-outputWriter.writerow(['Pressao', 'Data', 'Port', 'Fcnt'])
-outputWriter.writerow([dataFrame, timestamp, port, fcnt])
+outputWriter.writerow(['Dados HEX', 'Data', 'Port'])
+for l in range(0,len(jsonSorted)):
+    outputWriter.writerow([jsonSorted[l]["dataFrame"], jsonSorted[l]["timestamp"], jsonSorted[l]["port"]])
 outputFile.close()

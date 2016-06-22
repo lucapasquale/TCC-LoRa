21cd72fe3680ab6b932b6fca0e7e0ddf13215111
Luca Pasquale <lucapasquale@hotmail.com>
2016-06-22 12:01:33 -0300

    Iniciado WPF

---
+++ Teste Application/WpfApplication1/WpfApplication1/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace WpfApplication1
+{
+    using System;
+
+    using OxyPlot;
+    using OxyPlot.Series;
+
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            this.MyModel = new PlotModel { Title = "Example 1" };
+            this.MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+        }
+
+        public PlotModel MyModel { get; private set; }
+        
+    }
+}
---
+++ Teste Application/WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace WpfApplication1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var client = new RestClient();
+            client.BaseUrl = new Uri("https://artimar.orbiwise.com/rest/nodes/0004A30B001A674B/payloads/ul");
+            client.Authenticator = new HttpBasicAuthenticator("maua_ceun", "Maua2016");
+
+            var request = new RestRequest();           
+            IRestResponse response = client.Execute(request);
+
+            textBox.Text = response.Content;
+        }
+    }
+}

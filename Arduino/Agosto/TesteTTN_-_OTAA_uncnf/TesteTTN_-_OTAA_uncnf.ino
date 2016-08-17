7825e87052fb76a4f443a8b35de81a19f135b144
Luca Pasquale <lucapasquale.094@gmail.com>
2016-08-17 13:43:18 -0300

    Alterado biblioteca Arduino para TTN

--- Arduino/Agosto/TesteTTN_-_OTAA_uncnf/TesteTTN_-_OTAA_uncnf.ino
+++ Arduino/Agosto/TesteTTN_-_OTAA_uncnf/TesteTTN_-_OTAA_uncnf.ino
@@ -20,7 +20,7 @@ const byte devAddr[4] = {0x02, 0x01, 0x55, 0xB0};
 const byte nwkSKey[16] = {0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C};
 const byte appSKey[16] = {0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C};
 
-byte data[4];
+byte data[6];
 
 
 void setup() {
@@ -40,17 +40,30 @@ void setup() {
 }
 
 void loop() {
+  LeSensores();
+  ttu.sendBytes(data, sizeof(data), 10, false);
+  
+  for (int t = 0; t < 15 * 60; t++){
+    delay(1000);
+  }
+}
+
+void LeSensores() {
   int16_t temperature = dht.readTemperature() * 10;
   data[0] = highByte(temperature);
   data[1] = lowByte(temperature);
+  debugPrint("Temperatura: "); debugPrintLn(temperature);
   
   int16_t humidity = dht.readHumidity() * 10;
   data[2] = highByte(humidity);
   data[3] = lowByte(humidity);
+  debugPrint("Umidade: "); debugPrintLn(humidity);
 
-  debugPrint("Temperature: "); debugPrintLn(temperature);
-  debugPrint("Humidity: "); debugPrintLn(humidity);
- 
-  ttu.sendBytes(data, sizeof(data), 10, false);
-  delay(20000);
+  float vout = analogRead(A0);    //Le valor pino analogico sensor de pressao
+  vout = (vout * 5.0) / 1023.0;   //Converte valor 0-1023 para 0-5V
+  int16_t pres = round(222 * vout + 106);        //Converte o valor para hPa
+  data[4] = highByte(pres);
+  data[5] = lowByte(pres);
+  debugPrint("Pressao: "); debugPrintLn(pres);
+  }
 }
